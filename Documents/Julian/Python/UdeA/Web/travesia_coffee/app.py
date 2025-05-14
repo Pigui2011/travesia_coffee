b943e5b032d81a7b573d91e0b84625f13658ec75
Julian <pigui-1883@hotmail.com>
2025-05-14 08:37:49 -0500

    Reorganizar archivos en la raíz para despliegue en Render

--- Documents/Julian/Python/UdeA/Web/travesia_coffee/app.py
+++ Documents/Julian/Python/UdeA/Web/travesia_coffee/app.py
@@ -10,9 +10,45 @@ sdk = mercadopago.SDK("TEST-ACCESS-TOKEN")  # Reemplaza con tu access token
 def index():
     return render_template("index.html")
 
+# @app.route("/catalogo")
+# def catalogo():
+#     return render_template("catalogo.html")
+
 @app.route("/catalogo")
 def catalogo():
-    return render_template("catalogo.html")
+    productos = [
+        {
+            "nombre": "Café Clásico",
+            "precio": "$15.000",
+            "imagen": "img/producto1.jpeg",
+            "whatsapp": "https://wa.me/573002674845?text=Quiero%20comprar%20Café%20Clásico"
+        },
+        {
+            "nombre": "Café Seleccionado",
+            "precio": "$25.000",
+            "imagen": "img/producto2.jpeg",
+            "whatsapp": "https://wa.me/573002674845?text=Quiero%20comprar%20Café%20Seleccionado"
+        },
+        {
+            "nombre": "Café Molido Lb",
+            "precio": "$25.500",
+            "imagen": "img/producto3.jpeg",
+            "whatsapp": "https://wa.me/573002674845?text=Quiero%20comprar%20Café%20Seleccionado"
+        },
+        {
+            "nombre": "Café najulu Lb",
+            "precio": "$10.500",
+            "imagen": "img/producto4.jpeg",
+            "whatsapp": "https://wa.me/573002674845?text=Quiero%20comprar%20Café%20Seleccionado"
+        },
+        {
+            "nombre": "Café mañanas Lb",
+            "precio": "$18.500",
+            "imagen": "img/producto5.jpeg",
+            "whatsapp": "https://wa.me/573002674845?text=Quiero%20comprar%20Café%20Seleccionado"
+        }
+    ]
+    return render_template("catalogo.html", productos=productos)
 
 @app.route("/galeria")
 def galeria():
--- Documents/Julian/Python/UdeA/Web/travesia_coffee/cd travesia_coffee.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-result = subprocess.run(['git', '--version'], capture_output=True, text=True)
-print(result.stdout)
